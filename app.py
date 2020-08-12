9010d01cab9070a9a674a95c480472efdeeadf88
Quake Lai <quake.lai@insighteye.com.tw>
2020-08-12 18:09:53 +0800

    c'mon heroku

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def crud():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    # app.run()
+    app.run(host='0.0.0.0',port=os.environ['PORT']) # for Heroku
